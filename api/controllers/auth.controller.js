134b2c13cc5dab9fd8c1c00ca714903d5eddef13
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-05 22:03:36 +0500

    create middleware and a function to handle errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,30 +1,34 @@
-import mongoose from "mongoose";
-import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
-export const signup = async (req, res) => {
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
     !username ||
     !email ||
     !password ||
-    username === "" ||
-    email === "" ||
-    password === ""
+    username === '' ||
+    email === '' ||
+    password === ''
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, 'All fields are required'));
   }
-  const hashPassword = bcryptjs.hashSync(password, 10);
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
   const newUser = new User({
     username,
     email,
-    password: hashPassword,
+    password: hashedPassword,
   });
 
   try {
     await newUser.save();
-    res.json({ message: "Signup Successful" });
+    res.json('Signup successful');
+
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -18,8 +18,19 @@ app.use(express.json());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-  
+
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", auhtRoutes);
+
+
+app.use((err, req, res, next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message,
+  })
+});
