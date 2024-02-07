c23d49702b46bbf3b078a8056e5e733deb096651
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-07 16:02:52 +0500

    add google OAuth functionality

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -7,6 +7,7 @@ import {
   signInStart,
   signInSuccess,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 const Signin = () => {
   const [formData, setformData] = useState({});
@@ -98,6 +99,7 @@ const Signin = () => {
                 "Sign In"
               )}
             </Button>
+            <OAuth/>
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Dont have an account?</span>
