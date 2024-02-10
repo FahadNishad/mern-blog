11011e592c94e7ed85c5fe7ce54110b454c0ff9e
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-10 15:44:12 +0500

    add dark  mode functionality

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -45,7 +45,7 @@ const SignUp = () => {
     }
   };
   return (
-    <div className="min-h-screen mt-20">
+    <div className=" mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* this is for left side  */}
         <div className="flex-1">
