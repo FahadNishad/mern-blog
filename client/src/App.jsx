6aea76ea1182eed899e7686c9d9ecd7dfc8576e7
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-10 16:58:54 +0500

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import {Routes, Route, BrowserRouter} from 'react-router-dom'
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Signin from './Pages/Signin'
-import Signup from './Pages/SignUp'
-import Dashboard from './Pages/Dashboard'
-import Projects from './Pages/Projects'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Signin from "./Pages/Signin";
+import Signup from "./Pages/SignUp";
+import Dashboard from "./Pages/Dashboard";
+import Projects from "./Pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
 const App = () => {
   return (
-    <BrowserRouter >
-    <Header/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
-      <Route path='/sign-up' element={<Signup/>}/>
-      <Route path='/dashboard' element={<Dashboard/>}/>
-      <Route path='/projects' element={<Projects/>}/>
-    </Routes>
-    <Footer/>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route element={<PrivateRoute/>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
