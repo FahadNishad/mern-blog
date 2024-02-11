a9b72af9cf3e1e6603c3f28a74b59da72ca58248
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-11 18:36:00 +0500

    complete upload functionality user side

--- client/src/Pages/Signin.jsx
+++ client/src/Pages/Signin.jsx
@@ -10,106 +10,101 @@ import {
 import OAuth from "../components/OAuth";
 
 const Signin = () => {
-  const [formData, setformData] = useState({});
-  const { loading, error: errormessage } = useSelector((state) => state.user);
-  const navigate = useNavigate();
+  const [formData, setFormData] = useState({});
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const handleChange = (e) => {
-    setformData({ ...formData, [e.target.id]: e.target.value.trim() });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return dispatch(signInFailure("Please fill out all the fields"));
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
       dispatch(signInStart());
-      const res = await fetch("/api/auth/signin", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-
       if (data.success === false) {
         dispatch(signInFailure(data.message));
       }
-      console.log(data);
+
       if (res.ok) {
         dispatch(signInSuccess(data));
-        navigate("/");
+        navigate('/');
       }
     } catch (error) {
       dispatch(signInFailure(error.message));
     }
   };
   return (
-    <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
-        {/* this is for left side  */}
-        <div className="flex-1">
-          <Link
-            to="/"
-            className="font-bold dark:text-white text-3xl md:text-4xl"
-          >
-            <span className="px-2  py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Fahad's
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
+        {/* left */}
+        <div className='flex-1'>
+          <Link to='/' className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+              Sahand's
             </span>
             Blog
           </Link>
-          <p className="text-sm mt-5">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum
-            omnis dolore quibusdam veniam expedita hic beatae sunt totam
-            molestiae eum!
+          <p className='text-sm mt-5'>
+            This is a demo project. You can sign in with your email and password
+            or with Google.
           </p>
         </div>
+        {/* right */}
 
-        {/* this is for right side  */}
-        <div className="flex-1">
-          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-              <Label value="Your email"></Label>
+              <Label value='Your email' />
               <TextInput
-                type="email"
-                placeholder="name@company.com"
-                id="email"
+                type='email'
+                placeholder='name@company.com'
+                id='email'
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Your password"></Label>
+              <Label value='Your password' />
               <TextInput
-                type="password"
-                placeholder="*********"
-                id="password"
+                type='password'
+                placeholder='**********'
+                id='password'
                 onChange={handleChange}
               />
             </div>
             <Button
-              gradientDuoTone="purpleToPink"
-              type="submit"
-              disabled={loading && !errormessage}
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}
             >
-              {loading && !errormessage ? (
+              {loading ? (
                 <>
-                  <Spinner size="sm" />
-                  <span className="pl-3">Loading...</span>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
                 </>
               ) : (
-                "Sign In"
+                'Sign In'
               )}
             </Button>
-            <OAuth/>
+            <OAuth />
           </form>
-          <div className="flex gap-2 text-sm mt-5">
-            <span>Dont have an account?</span>
-            <Link to="/sign-up" className="text-blue-500">
-              Sign In
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Dont Have an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
             </Link>
           </div>
-          {errormessage && (
-            <Alert className="mt-5" color="failure">
-              {errormessage}
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
             </Alert>
           )}
         </div>
