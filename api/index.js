7cf62d554ab7806e853567b524cdcf815168da36
Fahad Ahmed Khan <fahadnishad124@gmail.com>
2024-02-11 14:52:05 +0500

    update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import auhtRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,22 +16,21 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-
 });
 
 app.use("/api/user", userRoutes);
 app.use("/api/auth", auhtRoutes);
 
-
-app.use((err, req, res, next)=>{
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
+  const message = err.message || "Internal Server Error";
   res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
-  })
+  });
 });
